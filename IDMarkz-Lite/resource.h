ffdcc4eefe4068d568010b2965751d22bbd3e5d9
William Peloquin <wpeloqui@gmail.com>
2020-07-26 20:10:56 -0500

    Initial Impound

---
+++ IDMarkz-Lite/resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by IDMarkz-Lite.rc
+//
+#define IDC_MYICON                      2
+#define IDD_IDMARKZLITE_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_IDMARKZLITE                 107
+#define IDI_SMALL                       108
+#define IDC_IDMARKZLITE                 109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_IDMARKZ_LITE         129
+#define IDC_BUTTON_BROWSE               1000
+#define IDC_RADIO_LOCATION              1001
+#define IDC_RADIO_SELECT_LOCATION       1002
+#define IDC_RADIO_REPLACE               1003
+#define IDC_RADIO_RENAME                1004
+#define IDC_RADIO_LO_RES_NEVER          1005
+#define IDC_RADIO_LO_RES_MISSING        1006
+#define IDC_RADIO_ALL                   1007
+#define IDC_CHECK_LO_RES_LINK           1008
+#define IDC_CHECK_EXPLORER              1009
+#define IDC_CHECK_DEFAULT_APP           1010
+#define ID_FILE_OPEN                    32771
+#define ID_FILE_CLOSE                   32772
+#define ID_FILE_EXPORTIDML              32773
+#define ID_FILE_PRINT                   32774
+#define ID_EDIT_PREFERENCES             32775
+#define ID_HELP_USERGUIDE               32776
+#define ID_HELP_ONLINETUTORIALS         32777
+#define ID_HELP_REPORTANISSUE           32778
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
